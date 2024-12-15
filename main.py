2bf9b55828cdf59bcd5e3825a87cec0f71e1410d
Yacine Tadjine <yacine.tadjine@campus.tu-berlin.de>
2024-12-15 00:00:00 +0000

    commit on 2024-12-15 00:00:00

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import os
+from datetime import datetime, timedelta
 from random import randint
 
-# Ensure that 'file.txt' is in the repository and contains initial data
+# Start- und Enddatum definieren
+start_date = datetime(2024, 12, 15)
+end_date = datetime(2025, 2, 26)
+
+# Anzahl der Tage im Bereich berechnen
+days_range = (end_date - start_date).days + 1
+
+# Datei initialisieren
 with open('file.txt', 'w') as file:
     file.write('Initial commit\n')
 
-for i in range(1, 365):
-    for j in range(0, randint(1, 10)):
-        d = str(i) + ' days ago'
-        
-        # Write the random date message to 'file.txt'
+for i in range(days_range):
+    commit_date = start_date + timedelta(days=i)
+    formatted_date = commit_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Zufällige Anzahl an Commits pro Tag
+    for _ in range(randint(1, 10)):
         with open('file.txt', 'a') as file:
-            file.write(d + '\n')
-        
-        # Add the changes
+            file.write(f'Commit am {formatted_date}\n')
+
         os.system('git add .')
-        
-        # Commit the changes with a date from 'd'
-        os.system(f'git commit --date="{d}" -m "commit"')
+        os.system(f'git commit --date="{formatted_date}" -m "commit on {formatted_date}"')
 
 # Push to GitHub
 os.system('git push -u origin main')
